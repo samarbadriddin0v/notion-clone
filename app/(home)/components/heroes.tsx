50c826fa32826bcaf3d436f6db51fc54315a7f93
samarbadriddin0v <sammibadriddinov@gmail.com>
2024-12-04 00:15:21 +0200

    Fix: Deployment

--- app/(home)/components/heroes.tsx
+++ app/(home)/components/heroes.tsx
@@ -1,69 +1,57 @@
-"use client";
+'use client'
 
-import { Button } from "@/components/ui/button";
-import { Loader } from "@/components/ui/loader";
-import { SignInButton } from "@clerk/clerk-react";
-import { useConvexAuth } from "convex/react";
-import { ArrowRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { Button } from '@/components/ui/button'
+import { Loader } from '@/components/ui/loader'
+import { SignInButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
 export const Heroes = () => {
-  const { isAuthenticated, isLoading } = useConvexAuth();
+	const { isAuthenticated, isLoading } = useConvexAuth()
 
-  return (
-    <>
-      <div className="max-w-3xl space-y-4">
-        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-          Write, plan, share. With AI at your side.
-        </h1>
-        <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-          Notion is the connected workspace where better, faster work happens.
-        </h3>
-        {isLoading && (
-          <div className="w-full flex justify-center items-center">
-            <Loader />
-          </div>
-        )}
+	return (
+		<>
+			<div className='max-w-3xl space-y-4'>
+				<h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>Write, plan, share. With AI at your side.</h1>
+				<h3 className='text-base sm:text-xl md:text-2xl font-medium'>
+					Notion is the connected workspace where better, faster work happens.
+				</h3>
+				{isLoading && (
+					<div className='w-full flex justify-center items-center'>
+						<Loader />
+					</div>
+				)}
 
-        {isAuthenticated && !isLoading && (
-          <Button asChild>
-            <Link href={"/documents"}>
-              Enter notion <ArrowRight className="h-4 w-4 ml-2" />
-            </Link>
-          </Button>
-        )}
+				{isAuthenticated && !isLoading && (
+					<Button asChild>
+						<Link href={'/documents'}>
+							Enter notion <ArrowRight className='h-4 w-4 ml-2' />
+						</Link>
+					</Button>
+				)}
 
-        {!isAuthenticated && !isLoading && (
-          <>
-            <SignInButton mode="modal">
-              <Button>
-                Get Notion Free <ArrowRight className="h-4 w-4 ml-2" />
-              </Button>
-            </SignInButton>
-          </>
-        )}
-      </div>
+				{!isAuthenticated && !isLoading && (
+					<>
+						<SignInButton mode='modal'>
+							<Button>
+								Get Notion Free <ArrowRight className='h-4 w-4 ml-2' />
+							</Button>
+						</SignInButton>
+					</>
+				)}
+			</div>
 
-      <div className="flex flex-col items-center justify-center max-w-5xl">
-        <div className="flex items-center">
-          <div className="relative h-[400px] w-[400px] hidden md:block">
-            <Image
-              src={"/men.svg"}
-              alt="Logo"
-              fill
-              className="object-cover dark:hidden"
-            />
-            <Image
-              src={"/men-dark.svg"}
-              alt="Logo"
-              fill
-              className="object-cover hidden dark:block"
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+			<div className='flex flex-col items-center justify-center max-w-5xl'>
+				<div className='flex items-center'>
+					<div className='relative h-[400px] w-[400px] hidden md:block'>
+						<Image src={'/men.svg'} alt='Logo' fill className='object-cover dark:hidden' />
+						<Image src={'/men-dark.svg'} alt='Logo' fill className='object-cover hidden dark:block' />
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,55 +1,55 @@
-"use client";
+'use client'
 
-import React from "react";
-import { Logo } from "./logo";
-import { ModeToggle } from "@/components/shared/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { useConvexAuth } from "convex/react";
-import Link from "next/link";
-import { Loader } from "@/components/ui/loader";
-import { useScrolled } from "@/hooks/use-scrolled";
+import React from 'react'
+import { Logo } from './logo'
+import { ModeToggle } from '@/components/shared/mode-toggle'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
+import { Loader } from '@/components/ui/loader'
+import { useScrolled } from '@/hooks/use-scrolled'
 
 export const Navbar = () => {
-  const { isAuthenticated, isLoading } = useConvexAuth();
-  const scrolled = useScrolled();
+	const { isAuthenticated, isLoading } = useConvexAuth()
+	const scrolled = useScrolled()
 
-  return (
-    <div
-      className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between",
-        scrolled && "border-b shadow-sm"
-      )}
-    >
-      <Logo />
-      <div className="flex items-center gap-x-2">
-        {isLoading && <Loader />}
+	return (
+		<div
+			className={cn(
+				'z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between',
+				scrolled && 'border-b shadow-sm'
+			)}
+		>
+			<Logo />
+			<div className='flex items-center gap-x-2'>
+				{isLoading && <Loader />}
 
-        {!isAuthenticated && !isLoading && (
-          <>
-            <SignInButton mode="modal">
-              <Button size={"sm"} variant={"ghost"}>
-                Log In
-              </Button>
-            </SignInButton>
-            <SignInButton mode="modal">
-              <Button size={"sm"}>Get Notion Free</Button>
-            </SignInButton>
-          </>
-        )}
+				{!isAuthenticated && !isLoading && (
+					<>
+						<SignInButton mode='modal'>
+							<Button size={'sm'} variant={'ghost'}>
+								Log In
+							</Button>
+						</SignInButton>
+						<SignInButton mode='modal'>
+							<Button size={'sm'}>Get Notion Free</Button>
+						</SignInButton>
+					</>
+				)}
 
-        {isAuthenticated && !isLoading && (
-          <>
-            <Button variant={"ghost"} size={"sm"} asChild>
-              <Link href={"/documents"}>Enter Notion</Link>
-            </Button>
-            <UserButton afterSignOutUrl="/" />
-          </>
-        )}
+				{isAuthenticated && !isLoading && (
+					<>
+						<Button variant={'ghost'} size={'sm'} asChild>
+							<Link href={'/documents'}>Enter Notion</Link>
+						</Button>
+						<UserButton afterSignOutUrl='/' />
+					</>
+				)}
 
-        <ModeToggle />
-      </div>
-    </div>
-  );
-};
+				<ModeToggle />
+			</div>
+		</div>
+	)
+}
